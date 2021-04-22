f91a6aae248f102e2d92e3bc8681426f9f8591c8
Mehul Lakhanpal <318097@gmail.com>
2021-04-22 22:07:08 +0530

    Codesandbox

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,17 @@ import ScrollComponent from "./components/ScrollComponent";
 import SpyScroll from "./components/SpyScroll";
 import ReactUI from "./components/ReactUI";
 import Home from "./components/Home";
+import Dropdown from "./components/Dropdown";
 
 const routes = [
   { route: "/spy-scroll", label: "Spy Scroll", component: SpyScroll },
   { route: "/scroll", label: "Scroll", component: ScrollComponent },
   { route: "/react-ui", label: "React UI", component: ReactUI },
+  {
+    route: "/toggle-dropdown",
+    label: "Close dropdown on outside click",
+    component: Dropdown
+  },
   { route: "/", label: "Home", component: Home }
 ];
 
